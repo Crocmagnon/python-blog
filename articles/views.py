0c1d0a0b29b39dec51fb2f0929fc249f499c761d
Gabriel Augendre <gabriel@augendre.info>
2020-08-14 16:19:02 +0200

    Only display published articles

--- articles/views.py
+++ articles/views.py
@@ -8,7 +8,13 @@ class ArticlesListView(generic.ListView):
     paginate_by = 15
     context_object_name = "articles"
 
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Article.PUBLISHED)
+
 
 class ArticleDetailView(generic.DetailView):
     model = Article
     context_object_name = "article"
+
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Article.PUBLISHED)
