6898bb22a572d75346a97280f5e4ce5eee5376dc
Gabriel Augendre <gabriel@augendre.info>
2020-08-28 21:49:15 +0200

    Display files urls and copy button

--- attachments/admin.py
+++ attachments/admin.py
@@ -6,5 +6,20 @@ from attachments.models import Attachment
 
 @register(Attachment)
 class AttachmentAdmin(admin.ModelAdmin):
-    list_display = ["description", "original_file", "processed_file"]
+    list_display = [
+        "description",
+        "original_file",
+        "original_file_url",
+        "processed_file",
+        "processed_file_url",
+    ]
     list_display_links = ["description"]
+
+    class Media:
+        js = ["attachments/js/copy_url.js"]
+
+    def processed_file_url(self, instance):
+        return instance.processed_file.url
+
+    def original_file_url(self, instance):
+        return instance.original_file.url
---
+++ attachments/static/attachments/js/copy_url.js
@@ -0,0 +1,17 @@
+function copy(data) {
+    navigator.clipboard.writeText(data).then(() => {
+        console.log("Copied");
+    })
+}
+
+$(document).ready(function() {
+    const $ = django.jQuery;
+    const fileUrls = $('td.field-processed_file_url, td.field-original_file_url');
+    for (let fileUrl of fileUrls) {
+        fileUrl = $(fileUrl);
+        const existingText = fileUrl.text();
+        const copyButton = `<a class='copy-button' href='#' onclick="copy(\'${existingText}\')">&#128203;</a>`;
+        let innerHTML = `<span>${existingText}</span> ${copyButton}`;
+        fileUrl.html(innerHTML);
+    }
+});
