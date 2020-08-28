6c3ab47a9b94d8ead7fa82a1d7b5f755a3972dcd
Gabriel Augendre <gabriel@augendre.info>
2020-08-28 22:09:50 +0200

    Remove onclick because CSP doesn't allow it

--- attachments/static/attachments/js/copy_url.js
+++ attachments/static/attachments/js/copy_url.js
@@ -1,5 +1,5 @@
 function copy(data) {
-    navigator.clipboard.writeText(data).then(() => {
+    navigator.clipboard.writeText(data.data).then(() => {
         console.log("Copied");
     })
 }
@@ -7,14 +7,18 @@ function copy(data) {
 $(document).ready(function() {
     const $ = django.jQuery;
     const fileUrls = $('td.field-processed_file_url, td.field-original_file_url');
+    let id = 0;
     for (let fileUrl of fileUrls) {
         fileUrl = $(fileUrl);
         const existingText = fileUrl.text().trim();
         if (!existingText) {
             continue;
         }
-        const copyButton = `<a class='copy-button' href='#' onclick="copy(\'${existingText}\')">&#128203;</a>`;
+        const buttonId = `copy-button-${id}`;
+        const copyButton = `<a class="copy-button" id="${buttonId}" href="#">&#128203;</a>`;
         let innerHTML = `<span>${existingText}</span> ${copyButton}`;
         fileUrl.html(innerHTML);
+        $(`#${buttonId}`).on("click", null, existingText, copy);
+        id++;
     }
 });
