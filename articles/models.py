48f9a2e468a13f8cd7068685c107aa3963705ea0
Gabriel Augendre <gabriel@augendre.info>
2020-11-11 12:07:06 +0100

    Remove unused method

--- articles/models.py
+++ articles/models.py
@@ -61,13 +61,6 @@ class Article(AdminUrlMixin, models.Model):
             type_ = "Page"
         return f"{self.title} ({type_})"
 
-    def get_admin_url(self):
-        content_type = ContentType.objects.get_for_model(self.__class__)
-        return reverse(
-            "admin:%s_%s_change" % (content_type.app_label, content_type.model),
-            args=(self.id,),
-        )
-
     def get_absolute_url(self):
         return reverse("article-detail", kwargs={"slug": self.slug})
 
