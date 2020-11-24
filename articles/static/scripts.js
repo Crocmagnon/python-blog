b99e63dbcde2c412ce5924c099ac1100a3a665e2
Gabriel Augendre <gabriel@augendre.info>
2020-11-24 18:28:47 +0100

    Allow "e" keyboard shortcut to edit a post

--- articles/static/scripts.js
+++ articles/static/scripts.js
@@ -17,3 +17,16 @@ function darkModeListener(e) {
 let mql = window.matchMedia("(prefers-color-scheme: dark)");
 darkModeListener(mql);
 mql.addListener(darkModeListener);
+
+window.onload = function () {
+    const adminLinkElement = document.querySelector(".article-detail .metadata a.admin-link");
+    if (adminLinkElement === undefined || adminLinkElement === null) {
+        return;
+    }
+    const adminLocation = adminLinkElement.href;
+    document.addEventListener("keydown", event => {
+        if (event.code === "KeyE") {
+            window.location = adminLocation;
+        }
+    })
+}
