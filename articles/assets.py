0949dc7c1d775d88ee239c2380d9122c5f729b96
Gabriel Augendre <gabriel@augendre.info>
2020-12-25 14:50:51 +0100

    Fix minification on chrome

--- articles/assets.py
+++ articles/assets.py
@@ -3,14 +3,14 @@ from django_assets import Bundle, register
 public_no_code = Bundle(
     "newcss.css",
     "public.css",
-    filters="cssmin",
+    filters="rcssmin",
     output="public_bundled.css",
 )
 public_with_code = Bundle(
     "newcss.css",
     "codehilite.css",
     "public.css",
-    filters="cssmin",
+    filters="rcssmin",
     output="public_code_bundled.css",
 )
 register("public_no_code", public_no_code)
