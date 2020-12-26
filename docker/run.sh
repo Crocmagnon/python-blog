0051cc3fa5df5876f04ddf17f1cbb80f44239ffc
Gabriel Augendre <gabriel@augendre.info>
2020-12-26 17:40:25 +0100

    Update run script

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-yes yes | python manage.py migrate \
-&& python manage.py collectstatic --noinput --clear \
-&& gunicorn blog.wsgi -b 0.0.0.0:8000 --log-file -
+set -euxo pipefail
+yes yes | python manage.py migrate
+python manage.py collectstatic --noinput --clear
+python manage.py assets build --manifest django
+gunicorn blog.wsgi -b 0.0.0.0:8000 --log-file -
