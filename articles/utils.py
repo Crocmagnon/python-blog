6b52f6b2b281753e3cb22722108250abce222236
Gabriel Augendre <gabriel@augendre.info>
2020-12-27 19:56:50 +0100

    Fix build full absolute url when no request

--- articles/utils.py
+++ articles/utils.py
@@ -5,4 +5,4 @@ def build_full_absolute_url(request, url):
     if request:
         return request.build_absolute_uri(url)
     else:
-        return (settings.BLOG["base_url"] + url).replace("//", "/")
+        return (settings.BLOG["base_url"] + url)[::-1].replace("//", "/", 1)[::-1]
