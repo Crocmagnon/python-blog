1b75896e8161a8ba0f8ce4fd78095d37bff6c676
Gabriel Augendre <gabriel@augendre.info>
2020-12-30 11:34:40 +0100

    Append slash to articles (redirect)

--- blog/urls.py
+++ blog/urls.py
@@ -33,7 +33,6 @@ urlpatterns = [
     path("drafts/", html.DraftsListView.as_view(), name="drafts-list"),
     path("feed/", feeds.CompleteFeed(), name="complete-feed"),
     path("api/render/<int:article_pk>/", api.render_article, name="api-render-article"),
-    path("<slug:slug>", html.ArticleDetailView.as_view(), name="article-detail-old"),
     path("<slug:slug>/", html.ArticleDetailView.as_view(), name="article-detail"),
 ]
 
