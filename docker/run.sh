29bace4395eb82a5f95285de2a4f1ccf95d7f4a2
Gabriel Augendre <gabriel@augendre.info>
2021-01-09 18:40:17 +0100

    Update run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eux
-yes yes | python manage.py migrate
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput --clear
 python manage.py compress
 gunicorn blog.wsgi -b 0.0.0.0:8000 --log-file -
