8d5edfd8dfa3733a64e871549a2422b8d22ad19d
Gabriel Augendre <gabriel@augendre.info>
2021-03-03 17:31:31 +0100

    Disable test skipping

--- articles/tests/test_admin.py
+++ articles/tests/test_admin.py
@@ -6,9 +6,10 @@ from articles.models import User
 
 
 @pytest.mark.django_db
-@pytest.mark.skip("Fails for no apparent reason")
+# @pytest.mark.skip("Fails for no apparent reason")
 @pytest.mark.flaky(reruns=5, reruns_delay=3)
 def test_can_access_add_article(client: Client, author: User):
     client.force_login(author)
-    res = client.get(reverse("admin:articles_article_add"))
+    url = reverse("admin:articles_article_add")
+    res = client.get(url)
     assert res.status_code == 200
