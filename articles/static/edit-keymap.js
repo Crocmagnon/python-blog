4cbdb0bc5458715a0a836a6e9cc7ab0bbc1bbacc
Gabriel Augendre <gabriel@augendre.info>
2021-03-21 15:39:31 +0100

    Use ES6 for custom script

--- articles/static/edit-keymap.js
+++ articles/static/edit-keymap.js
@@ -1,11 +1,10 @@
 'use strict';
-// Explicitely not using ES 6 features because the compressor doesn't support them.
 window.onload = function () {
-    var adminLinkElement = document.querySelector("a#admin-link");
+    const adminLinkElement = document.querySelector("a#admin-link");
     if (adminLinkElement === undefined || adminLinkElement === null) {
         return;
     }
-    var adminLocation = adminLinkElement.href;
+    const adminLocation = adminLinkElement.href;
     document.addEventListener("keydown", function(event) {
         if (event.code === "KeyE") {
             window.location = adminLocation;
