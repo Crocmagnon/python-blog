921994d2ada07ddd9b7c13a4b211e56cd76392e3
Gabriel Augendre <gabriel@augendre.info>
2021-04-10 16:27:45 +0200

    Add quadruple click to select all

---
+++ articles/static/copy-code.js
@@ -0,0 +1,28 @@
+"use strict";
+
+function addCopyCode() {
+    const codeBlocks = document.querySelectorAll("pre");
+    codeBlocks.forEach(pre => {
+        pre.addEventListener("click", event => {
+            if (event.detail === 4) {
+                const selection = window.getSelection();
+                selection.setBaseAndExtent(
+                    pre.querySelector("code").firstChild,
+                    0,
+                    pre.querySelector("code").lastChild,
+                    1
+                );
+            }
+        });
+        pre.setAttribute("title", "Quadruple click to select all");
+    });
+}
+
+
+((readyState) => {
+    if (readyState === "interactive") {
+        addCopyCode();
+    } else if (readyState === "loading") {
+        window.addEventListener("DOMContentLoaded", addCopyCode, false);
+    }
+})(document.readyState);
