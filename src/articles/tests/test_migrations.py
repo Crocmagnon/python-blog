61c6c8974fa8d2c1587657169257014e06317590
Gabriel Augendre <gabriel@augendre.info>
2022-05-25 19:19:37 +0200

    Fix test

--- src/articles/tests/test_migrations.py
+++ src/articles/tests/test_migrations.py
@@ -4,4 +4,4 @@ from django.core.management import call_command
 
 @pytest.mark.django_db()
 def test_missing_migrations() -> None:
-    call_command("makemigrations", "--check")
+    call_command("makemigrations", "--check", "--dry-run")
