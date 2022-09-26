3cf93a5ce661590dbf4d82dbfe170e4d663b25a6
Gabriel Augendre <gabriel@augendre.info>
2022-09-26 15:08:37 +0200

    Switch to argon2 for passwords

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -185,6 +185,11 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
